8ab607bbbbd3f3bdd297ba58c5f52bc3ee29f7dd
Janis Jansen <oss@janis.me>
2025-10-27 15:35:54 +0100

    chore(deps): Update all deps and taze config

--- taze.config.ts
+++ taze.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'taze';
 
 export default defineConfig({
   write: true,
+  recursive: true,
+  update: true,
   // run `npm install` or `yarn install` right after bumping
   install: true,
   ignorePaths: ['**/node_modules/**'],
