b2a3236a3ff27205901316cafa0cfc792fa2c082
Janis Jansen <oss@janis.me>
2025-11-27 14:08:47 +0100

    chore: Update release config to use relizy

---
+++ relizy.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'relizy';
+
+export default defineConfig({
+  monorepo: {
+    versionMode: 'selective',
+    packages: ['packages/*'],
+    ignorePackageNames: ['@surimi/docs'],
+  },
+
+  types: {
+    feat: { title: '🚀 Enhancements', semver: 'minor' },
+    perf: { title: '🔥 Performance', semver: 'patch' },
+    fix: { title: '🩹 Fixes', semver: 'patch' },
+    refactor: { title: '💅 Refactors', semver: 'patch' },
+    docs: { title: '📖 Documentation', semver: 'patch' },
+    build: { title: '📦 Build', semver: 'patch' },
+    types: { title: '🌊 Types', semver: 'patch' },
+    chore: { title: '🏡 Chore' },
+    examples: { title: '🏀 Examples' },
+    test: { title: '✅ Tests' },
+    style: { title: '🎨 Styles' },
+    ci: { title: '🤖 CI' },
+  },
+
+  changelog: {
+    rootChangelog: false,
+    formatCmd: 'pnpm format *.md',
+  },
+});
