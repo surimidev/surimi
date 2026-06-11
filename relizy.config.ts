02589d47d39669f164fd31925a91a29577b32c8d
Janis Jansen <oss@janis.me>
2026-06-11 07:09:25 +0200

    chore: fix release config

--- relizy.config.ts
+++ relizy.config.ts
@@ -10,22 +10,6 @@ export default defineConfig({
   publish: {
     access: 'public',
   },
-
-  types: {
-    feat: { title: '🚀 Enhancements', semver: 'minor' },
-    perf: { title: '🔥 Performance', semver: 'patch' },
-    fix: { title: '🩹 Fixes', semver: 'patch' },
-    refactor: { title: '💅 Refactors', semver: 'patch' },
-    docs: { title: '📖 Documentation', semver: 'patch' },
-    build: { title: '📦 Build', semver: 'patch' },
-    types: { title: '🌊 Types', semver: 'patch' },
-    chore: { title: '🏡 Chore' },
-    examples: { title: '🏀 Examples' },
-    test: { title: '✅ Tests' },
-    style: { title: '🎨 Styles' },
-    ci: { title: '🤖 CI' },
-  },
-
   changelog: {
     rootChangelog: false,
     formatCmd: 'pnpm format *.md',
